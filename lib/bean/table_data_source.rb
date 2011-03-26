da9af442e1585ce0b120b9864bfff777e70f5020
dan sinclair <dj2@everburning.com>
2011-03-25 23:26:46 -0400

    move TableDataSource into Bean module
    - add sort for data source

--- lib/bean/table_data_source.rb
+++ lib/bean/table_data_source.rb
@@ -1,21 +1,28 @@
-class TableDataSource
-  attr_reader :data
+module Bean
+  class TableDataSource
+    attr_reader :data
 
-  def initialize(data)
-    @data = data
-  end
+    def initialize(data)
+      @data = data
+    end
 
-  def numberOfRowsInTableView(tableView)
-    data.length
-  end
+    def numberOfRowsInTableView(tableView)
+      data.length
+    end
 
-  def tableView(view, objectValueForTableColumn:column, row:i)
-    ident = column.identifier
+    def tableView(view, objectValueForTableColumn:column, row:i)
+      ident = column.identifier
 
-    if data[i].respond_to?(:[])
-      data[i][ident.intern]
-    elsif data[i].respond_to?(ident.to_sym)
-      data[i].send(ident.to_sym)
+      if data[i].respond_to?(:[])
+        data[i][ident.intern]
+      elsif data[i].respond_to?(ident.to_sym)
+        data[i].send(ident.to_sym)
+      end
+    end
+
+    def tableView(table, sortDescriptorsDidChange:old_descriptors)
+      data.sortUsingDescriptors(table.sortDescriptors)
+      table.reloadData
     end
   end
 end
